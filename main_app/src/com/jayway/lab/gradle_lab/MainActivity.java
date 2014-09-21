7e227d0401896df38b56a770d17cdf1b4d3166eb
Karin Hofbauer <Albin.Theander@jayway.com>
2014-09-21 21:22:26 +0200

    First main app

--- main_app/src/com/jayway/lab/gradle_lab/MainActivity.java
+++ main_app/src/com/jayway/lab/gradle_lab/MainActivity.java
@@ -1,9 +1,14 @@
 package com.jayway.lab.gradle_lab;
 
+import java.util.Random;
+
+import com.jayway.labs.gradle.supermath.Adder;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,21 +17,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showAddition();
     }
 
 
-    @Override
+    private void showAddition() {
+		Adder adder = new Adder();
+		Random random = new Random();
+		int i1 = random.nextInt(100);
+		int i2 = random.nextInt(100);
+		int i3 = random.nextInt(100);
+		int sum = adder.add(i1, i2, i3);
+		TextView textView = (TextView) findViewById(R.id.addition);
+		textView.setText(i1 + " + " + i2 + " + " + i3 + " = " + sum);
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
